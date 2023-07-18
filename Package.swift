bff364c2ab8a29e56607d10fb81933ac0ed42ed3
Jordan Kay <jordan@fleuronic.com>
2023-07-18 17:04:46 -0400

    Update submodules

--- Package.swift
+++ Package.swift
@@ -1,8 +1,14 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "Diesel",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
     products: [
         .library(
             name: "Diesel",
