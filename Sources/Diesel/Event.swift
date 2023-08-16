dde5ca7efa56617e3a72aa203a6a4bcaf1dc7650
Jordan Kay <jordan@fleuronic.com>
2023-08-16 15:11:55 -0400

    Add Show model

--- Sources/Diesel/Event.swift
+++ Sources/Diesel/Event.swift
@@ -3,18 +3,15 @@
 import struct Foundation.Date
 
 public struct Event {
-	public let name: String?
 	public let slug: String?
 	public let date: Date
 	public let timeZone: String
 
 	public init(
-		name: String?,
 		slug: String?,
 		date: Date,
 		timeZone: String
 	) {
-		self.name = name
 		self.slug = slug
 		self.date = date
 		self.timeZone = timeZone
---
+++ Sources/Diesel/Show.swift
@@ -0,0 +1,9 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+public struct Show {
+	public let name: String
+
+	public init(name: String) {
+		self.name = name
+	}
+}
